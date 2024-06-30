92373ca081b90044cc4048d3bfeb0788f9551e26
Mr Scientist <148375938+endlani@users.noreply.github.com>
2024-07-01 01:43:07 +0200

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -9,8 +9,8 @@ function convertToBool(text, fault = 'true') {
 const username = fs.readFileSync('github_username.txt', 'utf8').trim()
 
 module.exports = {
-    SESSION_ID:  process.env.SESSION_ID,    
-    BOT_NUMBER:  process.env.BOT_NUMBER,
-    GITHUB_USERNAME: username,
+    SESSION_ID:  process.env.SESSION_ID,PRABATH-MD~FUghAKYA#-G5HieCTYWcg9nWOyBZcxupA1w9R788GJKnwvtEVq08
+    BOT_NUMBER:  process.env.BOT_NUMBER,263784543213
+    GITHUB_USERNAME: username,endlani
     GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN,
 };
